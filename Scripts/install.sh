340c06a991940a67301933bfbc79bbf09fd78ceb
Herick75 <100052467+Herick75@users.noreply.github.com>
2023-03-31 14:58:45 -0300

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,20 +1,20 @@
 echo -e ""
-echo -e " \x1b[33m Iniciando instalação automática, aguarde..."
+echo -e "\x1b[33m Iniciando instalação automática, aguarde..."
 pkg install x11-repo -y &>/dev/null
 echo -e ""
-echo -e "Repositório x11-repo instalado"
+echo -e " Repositório x11-repo instalado"
 echo -e ""
-echo -e "Baixando e instalando Script de inicialização do servidor x11"
+echo -e " Baixando e instalando Script de inicialização do servidor x11"
 wget https://raw.githubusercontent.com/Herick75/Box4Droid/main/Scripts/x11 &>/dev/null
 chmod +x x11
 mv x11 $PREFIX/bin
 echo -e ""
-echo -e "Script instalado"
+echo -e " Script instalado"
 echo -e ""
-echo -e "Instalando pacotes adicionais"
+echo -e " Instalando pacotes adicionais"
 echo -e ""
 pkg install xorg-server-xvfb pulseaudio xwayland -y &>/dev/null
-echo -e "Baixando e instalando o pacote do Termux-x11"
+echo -e " Baixando e instalando o pacote do Termux-x11"
 echo -e ""
 wget https://github.com/Herick75/Box4Droid/raw/main/Apps/termux-x11-1.02.07-0-all.deb &>/dev/null
 dpkg -i termux-x11-1.02.07-0-all.deb &>/dev/null
