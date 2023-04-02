f2bd91ba61a8a7c17f1563137d45d702f1c510c7
Herick75 <100052467+Herick75@users.noreply.github.com>
2023-04-02 17:51:21 -0300

    Add files via upload

---
+++ Scripts/start-ubuntu.sh
@@ -0,0 +1,30 @@
+#!/data/data/com.termux/files/usr/bin/bash
+cd $(dirname $0)
+pulseaudio --start
+## For rooted user: pulseaudio --start --system
+## unset LD_PRELOAD in case termux-exec is installed
+unset LD_PRELOAD
+command="proot"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r ubuntu-fs"
+command+=" -b /dev"
+command+=" -b /proc"
+command+=" -b ubuntu-fs/root:/dev/shm"
+## uncomment the following line to have access to the home directory of termux
+#command+=" -b /data/data/com.termux/files/home:/root"
+## uncomment the following line to mount /sdcard directly to / 
+command+=" -b /sdcard"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=$TERM"
+command+=" LANG=C.UTF-8"
+command+=" /bin/bash --login"
+com="$@"
+if [ -z "$1" ];then
+    exec $command
+else
+    $command -c "$com"
+fi
