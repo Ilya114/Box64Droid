6f358937bd5ac9d1613e2f1071f597635d792f5f
Ilya114 <93242944+Ilya114@users.noreply.github.com>
2024-02-03 15:57:15 +0200

    Update root.py

--- installers/root.py
+++ installers/root.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, time
 def packages():
     os.system("pkg install x11-repo -y &>/dev/null")
     os.system("pkg install pulseaudio wget tsu xkeyboard-config termux-x11-nightly termux-am -y  &>/dev/null")
@@ -29,7 +29,7 @@ def clear_waste():
     os.system("clear")
 def storage():
     os.system("termux-setup-storage")
-    os.system("sleep 2")
+    time.sleep(2)
 os.system("clear")
 print(" Starting Box64Droid (root version) installation... Please allow storage permission!")
 storage()
