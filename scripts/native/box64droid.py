1dce08232aa3cf6cdd872cd4d885d609aa2ecdb5
Ilya114 <93242944+Ilya114@users.noreply.github.com>
2024-02-06 18:13:18 +0200

    Update box64droid.py

--- scripts/native/box64droid.py
+++ scripts/native/box64droid.py
@@ -1,6 +1,7 @@
 import os, time, shutil, sys
 def start_box64droid():
     os.system("clear")
+    del os.environ("LD_PRELOAD")
     print("Starting Termux-X11...")
     os.system("termux-x11 :0 &>/dev/null &")
     print("Starting PulseAudio...")
