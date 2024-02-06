c52fd50004b217859bc02205c349debe9d314902
Ilya114 <93242944+Ilya114@users.noreply.github.com>
2024-02-06 14:34:58 +0200

    Update box64droid.py

--- scripts/native/box64droid.py
+++ scripts/native/box64droid.py
@@ -55,7 +55,7 @@ def main_menu():
     print("6) Exit")
     print("")
     choice = input()
-    if choice != "1" and choice != "2" and choice != "3" and choice != "4" and choice != "5" and choice != "6" and choice != "7":
+    if choice != "1" and choice != "2" and choice != "3" and choice != "4" and choice != "5" and choice != "6":
         print("Incorrect or empty option!")
         main_menu()
     elif choice == "1":
