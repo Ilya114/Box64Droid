d872b8aa9dbcf270f2ced627e3775bf3504b5c6f
Ilya114 <93242944+Ilya114@users.noreply.github.com>
2024-02-22 11:24:42 +0200

    Update box64droid.py

--- scripts/native/box64droid.py
+++ scripts/native/box64droid.py
@@ -157,7 +157,7 @@ def main_menu():
         os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/box64droid.py &>/dev/null")
         os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/start-box64.py &>/dev/null")
         os.system("mv box64droid.py start-box64.py $PREFIX/bin/")
-        os.system("python3 $PREFIX/bin/box64droid.py")
+        os.system("python3 $PREFIX/bin/box64droid.py --start")
         exit()
     elif choice == "7":
         os.system("clear")
