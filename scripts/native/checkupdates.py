6516783e11cf5dc64c5536e5dcfc894243794956
Ilya114 <93242944+Ilya114@users.noreply.github.com>
2024-02-24 16:11:30 +0200

    Update checkupdates.py

--- scripts/native/checkupdates.py
+++ scripts/native/checkupdates.py
@@ -1,17 +1,16 @@
 import os
 from box64droid import ver
-def check():
-    print("Checking for starting scripts updates...")
-    ver2=240225
-    if ver != ver2:
-        os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/box64droid.py &>/dev/null")
-        os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/start-box64.py &>/dev/null")
-        os.system("mv box64droid.py start-box64.py $PREFIX/bin/")
-        print("Update done!")
-    else:
-        print("Updates not found")
-    os.system("sleep 2")
-    os.system("rm $PREFIX/bin/checkupdates.py")
-    os.system("python3 $PREFIX/bin/box64droid.py --start")
-    exit()
-check()
+print("Checking for starting scripts updates...")
+ver2=240226
+if ver != ver2:
+    os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/box64droid.py &>/dev/null")
+    os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/start-box64.py &>/dev/null")
+    os.system("mv box64droid.py start-box64.py $PREFIX/bin/")
+    print("Update done!")
+else:
+    print("Updates not found")
+os.system("sleep 2")
+os.system("rm $PREFIX/bin/checkupdates.py")
+os.system("python3 $PREFIX/bin/box64droid.py --start")
+exit()
+
