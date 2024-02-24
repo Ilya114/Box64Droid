696b4e79736f715712757281ebaa601b94389ea5
Ilya114 <93242944+Ilya114@users.noreply.github.com>
2024-02-24 16:59:08 +0200

    Update checkupdates.py

--- scripts/native/checkupdates.py
+++ scripts/native/checkupdates.py
@@ -1,7 +1,7 @@
 import os
 from box64droid import ver
 print("Checking for starting scripts updates...")
-ver2=240226
+ver2=240227
 if ver != ver2:
     os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/box64droid.py &>/dev/null")
     os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/start-box64.py &>/dev/null")
