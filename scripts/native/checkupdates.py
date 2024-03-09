06be760a67aa42e6d9480156d8229d79993583b8
Ilya114 <93242944+Ilya114@users.noreply.github.com>
2024-03-09 07:51:55 +0200

    Update checkupdates.py

--- scripts/native/checkupdates.py
+++ scripts/native/checkupdates.py
@@ -3,10 +3,13 @@ from box64droid import ver
 print("Checking for Box64Droid updates...")
 ver2=90324
 if ver != ver2:
-    print("New update available! Installing...")
+    print("New update available! Updating scripts...")
     os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/box64droid.py &>/dev/null")
     os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/start-box64.py &>/dev/null")
     os.system("mv box64droid.py start-box64.py $PREFIX/bin/")
+    print("Updating glibc-prefix...")
+    os.system("wget -q --show-progress https://github.com/Ilya114/Box64Droid/releases/download/alpha/glibc-prefix.tar.xz")
+    os.system("tar -xJf glibc-prefix.tar.xz -C $PREFIX/")
     print("Update done!")
     print("Changes:")
     print("- Updated Turnip (64 bit, fixed Adreno 750)")
