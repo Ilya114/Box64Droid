8d1d673d16afee1e54da79f19184d16297a76636
Ilya114 <93242944+Ilya114@users.noreply.github.com>
2024-03-17 17:30:03 +0200

    Update native.py

--- installers/native.py
+++ installers/native.py
@@ -1,7 +1,7 @@
 import os, shutil, time
 def packages():
-    os.system("pkg install x11-repo -y &>/dev/null")
-    os.system("pkg install pulseaudio wget xkeyboard-config freetype fontconfig libpng xorg-xrandr termux-x11-nightly termux-am zenity which bash curl sed cabextract -y --no-install-recommends &>/dev/null")
+    os.system("pkg install x11-repo glibc-repo -y &>/dev/null")
+    os.system("pkg install pulseaudio wget glibc xkeyboard-config freetype fontconfig libpng xorg-xrandr termux-x11-nightly termux-am zenity which bash curl sed cabextract -y --no-install-recommends &>/dev/null")
 def check_prev_version():
     prefix = "/data/data/com.termux/files/home/.wine"
     glibc = "/data/data/com.termux/files/usr/glibc"
