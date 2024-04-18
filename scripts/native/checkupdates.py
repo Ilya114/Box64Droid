108b01cc47ac34234e77eaa37b0af5a471842f91
Ilya114 <93242944+Ilya114@users.noreply.github.com>
2024-04-18 21:51:03 +0300

    Update checkupdates.py

--- scripts/native/checkupdates.py
+++ scripts/native/checkupdates.py
@@ -1,21 +1,20 @@
 import os
 from box64droid import ver
 print("Checking for Box64Droid updates...")
-ver2=604243
+ver2=180424
 if ver != ver2:
     print("New update available! Updating scripts...")
     os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/box64droid.py &>/dev/null")
     os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/start-box64.py &>/dev/null")
     os.system("mv box64droid.py start-box64.py $PREFIX/bin/")
-    #print("Updating glibc-prefix...")
-    #os.system("wget -q --show-progress https://github.com/Ilya114/Box64Droid/releases/download/alpha/glibc-prefix.tar.xz")
-    #os.system("tar -xJf glibc-prefix.tar.xz -C $PREFIX/")
-    #os.system("rm glibc-prefix.tar.xz")
+    print("Updating glibc-prefix...")
+    os.system("wget -q --show-progress https://github.com/Ilya114/Box64Droid/releases/download/alpha/glibc-prefix.tar.xz")
+    os.system("tar -xJf glibc-prefix.tar.xz -C $PREFIX/")
+    os.system("rm glibc-prefix.tar.xz")
     print("Update done!")
-    print("Changes (01.04.24):")
+    print("Changes (18.04.24):")
     print("- Updated Box64")
-    print("- Updated Turnip (64 bit)")
-    print('- Now config folder named "Box64Droid (native)" (@Th_Fvck asked)')
+    print("- Replaced Wine 9.5 by 9.6")
 else:
     print("Updates not found")
 os.system("sleep 2")
