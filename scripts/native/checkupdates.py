292a8c9e085335e29b241153a0d4558d4cd13454
Ilya114 <93242944+Ilya114@users.noreply.github.com>
2024-07-27 16:07:26 +0300

    Update checkupdates.py

--- scripts/native/checkupdates.py
+++ scripts/native/checkupdates.py
@@ -1,16 +1,16 @@
 import os
 from box64droid import ver
 print("Checking for Box64Droid updates...")
-ver2=270724
+ver2=2707242
 if ver != ver2:
-    print("New update available! Updating glibc-prefix...")
-    #os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/box64droid.py &>/dev/null")
-    #os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/start-box64.py &>/dev/null")
-    #os.system("mv box64droid.py start-box64.py $PREFIX/bin/")
+    print("New update available! Updating scripts...")
+    os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/box64droid.py &>/dev/null")
+    os.system("wget https://raw.githubusercontent.com/Ilya114/Box64Droid/main/scripts/native/start-box64.py &>/dev/null")
+    os.system("mv box64droid.py start-box64.py $PREFIX/bin/")
     #print("Updating glibc-prefix...")
-    os.system("wget -q --show-progress https://github.com/Ilya114/Box64Droid/releases/download/alpha/glibc-prefix.tar.xz")
-    os.system("tar -xJf glibc-prefix.tar.xz -C $PREFIX/")
-    os.system("rm glibc-prefix.tar.xz")
+    #os.system("wget -q --show-progress https://github.com/Ilya114/Box64Droid/releases/download/alpha/glibc-prefix.tar.xz")
+    #os.system("tar -xJf glibc-prefix.tar.xz -C $PREFIX/")
+    #os.system("rm glibc-prefix.tar.xz")
     print("Update done!")
     print("Changes (27.07.24):")
     print("- Replaced Wine 9.6 by 9.7 and added 9.13")
