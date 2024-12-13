46aea1ce598c5874df23efd49a1ae96d097d0f5b
Ilya114 <93242944+Ilya114@users.noreply.github.com>
2024-12-13 14:01:23 +0200

    Start work on Hangover version

--- installers/install.sh
+++ installers/install.sh
@@ -10,13 +10,14 @@ echo "Hello, this is a Box64Droid installer, please select need version to insta
 echo ""
 echo "Actual version:"
 echo "1) Native (Adreno 610-750, Android 10+)"
+echo "2) Hangover (All GPU's, WIP (not able to install now))
 echo ""
 echo "Irrelevant versions:"
-echo "2) Non-root (Adreno 610-750, Android 12+)"
-echo "3) Root (Adreno 610-750, Android 10+)"
-echo "4) VirGL (Other GPU's, based on non-root, Android 12+)"
+echo "3) Non-root (Adreno 610-750, Android 12+)"
+echo "4) Root (Adreno 610-750, Android 10+)"
+echo "5) VirGL (Other GPU's, based on non-root, Android 12+)"
 echo ""
-echo "5) Cancel the Box64Droid installation"
+echo "6) Cancel the Box64Droid installation"
 echo ""
 read version
 if [ -z $version ]
@@ -27,16 +28,16 @@ then
 elif [ $version = 1 ]
 then
     curl -o native.py https://raw.githubusercontent.com/Ilya114/Box64Droid/main/installers/native.py && python3 native.py
-elif [ $version = 2 ]
-then
-    curl -o non-root.py https://raw.githubusercontent.com/Ilya114/Box64Droid/main/installers/non-root.py && python3 non-root.py
 elif [ $version = 3 ]
 then
-    curl -o root.py https://raw.githubusercontent.com/Ilya114/Box64Droid/main/installers/root.py && python3 root.py
+    curl -o non-root.py https://raw.githubusercontent.com/Ilya114/Box64Droid/main/installers/non-root.py && python3 non-root.py
 elif [ $version = 4 ]
 then
-    curl -o virgl.py https://raw.githubusercontent.com/Ilya114/Box64Droid/main/installers/virgl.py && python3 virgl.py
+    curl -o root.py https://raw.githubusercontent.com/Ilya114/Box64Droid/main/installers/root.py && python3 root.py
 elif [ $version = 5 ]
+then
+    curl -o virgl.py https://raw.githubusercontent.com/Ilya114/Box64Droid/main/installers/virgl.py && python3 virgl.py
+elif [ $version = 6 ]
 then
     rm install
     exit
